082e05ff285b36f44f327b03401cf1889601a6de
mlacher <mlacher@live.de>
2018-01-10 14:02:29 +0100

    2nd update
    included XRP Ripple to bib
    
    started EMA/MACD calculation, not wokring

--- Crypto.R
+++ Crypto.R
@@ -2,20 +2,24 @@
 
 install.packages("Quandl")
 install.packages("dygraphs")
+
 library(xts)  
 library("Quandl")
 library("dygraphs")
 # Quandl.api_key('***************')
 BTC <- Quandl("BITFINEX/BTCUSD",type="xts")
-ETH <- Quandl("BITFINEX/ETHUSD")
+ETH <- Quandl("BINANCE/ETHUSD")
 IOTA <- Quandl("BITFINEX/IOTUSD")
+XRP <- Quandl("BITFINEX/XRPUSD")
+#XVG <- Quandl("BITFINEX/XVGUSD")
+
 test <- as.matrix(ETH[2])
-
-BTC.ma11 <- xts(x= filter(BTC[,3], rep(1/11, 11)),order.by = BTC)
-BTC<-cbind.xts(BTC.ma11,BTC) # name missing, order wrong
-
+BTC.ma7 <- filter(BTC[,3], 1/13,sides = 1, method = "recursive") # smoothing 1 week
+BTC.ma11 <- filter(BTC[,3], 2/27,sides = 1, method = "recursive" ) # smoothing half month
+BTC<-cbind.xts(BTC.ma7,BTC) # name missing 
+BTC<-cbind.xts(BTC.ma11,BTC) # name missing
 # Plot
-dygraph(BTC[,1:2]) %>% 
+dygraph(BTC[,1:3]) %>% 
   dyRangeSelector()
 
 
