c7f1bf6f7d20f49f6bb9a799f8d54a0d02fcdd0d
mlacher <mlacher@live.de>
2018-01-20 22:45:43 +0100

    major update on structure
    open issues with plots

--- Cryp_package.R
+++
@@ -1,2 +0,0 @@
-install.packages("dygraphs")
-library("xts")  
--- Crypto.R
+++ Crypto.R
@@ -4,6 +4,7 @@
 install.packages("xts")
 install.packages("Quandl")
 install.packages("dygraphs")
+install.packages("digest")
 install.packages("TTR")
 
 library("xts")  
@@ -14,35 +15,45 @@ library("dygraphs")
 #------------------------------Functions-----------------------------#
 
 #1.1 Read Database
-
 Read_Crypt_DB<- function(Crypt_Name){
 Crypt_Add = paste("BITFINEX/",Crypt_Name,"USD", sep ="")
-BTC <- Quandl(Crypt_Add,type="xts")
-return (BTC)
+Crypt_DB <- Quandl(Crypt_Add,type="xts")
+return (Crypt_DB)
 }
-
 
 #1.2 Calulate Stuff
 Calc_Crypt <- function (Crypt_DB){
 macd  <- MACD(Crypt_DB[,2], 12, 26, 9, maType="EMA" )
-EMA12 <- EMA(BTC[,3], 12)
-EMA26 <- EMA(BTC[,3], 20)
+EMA12 <- EMA(Crypt_DB[,3], 12)
+EMA26 <- EMA(Crypt_DB[,3], 20)
 EMA <- cbind(EMA12,EMA26)
-colnames(EMA.BTC)<- c("EMA12","EMA26")
-Crypt_DB<-cbind.xts(EMA,Crypt_DB) 
+colnames(EMA)<- c("EMA12","EMA26")
+cCrypt_DB<-cbind.xts(EMA,Crypt_DB) 
+return (cCrypt_DB)
 }
+
+#1.3 Plot Range and Save
+Plot_Crypt <- function (cCrypt_DB, begDate, endDate){
+Date_Add = paste(begDate,"/",endDate, sep = "")
+plot_cCrypt_DB <- cCrypt_DB[Date_Add]
+windows()
+savePlot(pdf,'myplot.pdf')
+par(mfrow=c(2,1))
+plot(plot_cCrypt_DB[,3])
+plot(plot_cCrypt_DB[,c(1,2)])
+dev.off()
+}
+
+macd <- macd["2017-6/2018-6"] 
+
+
 #------------------------------Main----------------------------------#
 
-BTC <- Read_Crypt_DB("BTC")
-Calc_Crypt(BTC)
+BTC  <- Read_Crypt_DB("BTC")
+cBTC <- Calc_Crypt(BTC)
+Plot_Crypt(cBTC,"2017-6","2018-1")
 # Plot
 
-dygraph(BTC[,c(1,2,5)]) %>% 
+dygraph(cBTC[,c(1,2,5)]) %>% 
   dyRangeSelector()
 
-BTC <- BTC["2017-6/2018-6"]
-macd <- macd["2017-6/2018-6"] 
-windows()
-par(mfrow=c(2,1))
-plot(macd)
-plot(BTC[,c(1,2,5)])
