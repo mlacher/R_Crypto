a1b1ad65b30863d5de8a5c0a09ecc01e1048e048
mlacher <mlacher@live.de>
2018-01-23 16:38:42 +0100

    Included Package
    started calulation package

---
+++ StockCalc/R/CryptoCalc.R
@@ -0,0 +1,51 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+library("xts")
+library("Quandl")
+library("TTR")
+library("dygraphs")
+
+#------------------------------Functions-----------------------------#
+
+#1.1 Read Database
+Read_Crypt_DB<- function(Crypt_Name){
+  Crypt_Add = paste("BITFINEX/",Crypt_Name,"USD", sep ="")
+  Crypt_DB <- Quandl(Crypt_Add,type="xts")
+  return (Crypt_DB)
+}
+
+#1.2 Calulate Stuff
+Calc_Crypt <- function (Crypt_DB){
+  macd  <- MACD(Crypt_DB[,2], 12, 26, 9, maType="EMA" )
+  EMA12 <- EMA(Crypt_DB[,3], 12)
+  EMA26 <- EMA(Crypt_DB[,3], 20)
+  EMA <- cbind(EMA12,EMA26)
+  colnames(EMA)<- c("EMA12","EMA26")
+  cCrypt_DB<-cbind.xts(EMA,Crypt_DB)
+  return (cCrypt_DB)
+}
+
+#1.3 Plot Range and Save
+Plot_Crypt <- function (cCrypt_DB, begDate, endDate){
+  Date_Add = paste(begDate,"/",endDate, sep = "")
+  plot_cCrypt_DB <- cCrypt_DB[Date_Add]
+  windows()
+  savePlot(pdf,'myplot.pdf')
+  par(mfrow=c(2,1))
+  plot(plot_cCrypt_DB[,3])
+  plot(plot_cCrypt_DB[,c(1,2)])
+  dev.off()
+}
