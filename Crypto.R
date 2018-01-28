d020ab4a69f5c39c8321a283ab65235d3891470b
mlacher <mlacher@live.de>
2018-01-28 19:07:59 +0100

    better overview

--- Crypto.R
+++ Crypto.R
@@ -14,17 +14,17 @@ library("dygraphs")
 
 #------------------------------Functions-----------------------------#
 
-#1.1 Read Database
+#-----------------------1.1 Read Database----------------------------#
 Read_Crypt_DB<- function(Crypt_Name){
 Crypt_Add = paste("BITFINEX/",Crypt_Name,"USD", sep ="")
 Crypt_DB <- Quandl(Crypt_Add,type="xts")
 return (Crypt_DB)
 }
 
-#1.2 Calulate Stuff
+#----------------------1.2 Calulate Stuff----------------------------#
 Calc_Crypt <- function (Crypt_DB){
 
-#1.2.1 MACD / EMA
+#-----------------------1.2.1 MACD / EMA-----------------------------#
 # MACD 
 # Long Buy Signal : EMA12>EMA26 cross from below
 # Sell Signal     : EMA12<EMA26 cross from top
@@ -35,7 +35,7 @@ EMA <- cbind(EMA12,EMA26)
 colnames(EMA)<- c("EMA12","EMA26")
 cCrypt_DB<-cbind.xts(EMA,Crypt_DB) 
 
-#1.2.2 Boilinger Band
+#---------------------1.2.2 Boilinger Band---------------------------#
 # Trend up    : Chart crosses Upper band
 # Trend low   : Chart crosses Lower band
 # Trend Change short: Chart close to one band -> leads to other direction 
@@ -43,23 +43,23 @@ cCrypt_DB<-cbind.xts(EMA,Crypt_DB)
 BB20<- BBands(Crypt_DB[,3], sd=2.0)
 cCrypt_DB<-cbind.xts(BB20,cCrypt_DB) 
 
-#1.2.3 RSI
+#--------------------------1.2.3 RSI---------------------------------#
 # Undervalued   : RSI< 30
 # Overvalued    : RSI> 70
 Rsi<-RSI(Crypt_DB[,3])
 cCrypt_DB<-cbind.xts(Rsi,cCrypt_DB)
 
-#1.2.X Return XTS Object
+#--------------1.2.X Return XTS Object-------------------------------#
 # Return Object with following Structure:
 # EMA|BBdown|BBAVG|BBup|pctB|EMA12|EMA26|HIGH|LOW|MID|LAST|BID|ASK|Vol
 return (cCrypt_DB)
 }
 
-#1.3 BuySignal
+#--------------------1.3 BuySignal-----------------------------------#
 # Algorithm to calculate WinSuccess 
 # Include BB(Bandwith small)+ RSI(70-30)+MACD(positiv)+BB(more uptrend)
 
-#1.4 Plot Range and Save
+#----------------1.4 Plot Range and Save-----------------------------#
 Plot_Crypt <- function (cCrypt_DB, begDate, endDate){
 Date_Add = paste(begDate,"/",endDate, sep = "")
 plot_cCrypt_DB <- cCrypt_DB[Date_Add]
@@ -84,6 +84,7 @@ pcETH <- Plot_Crypt(cETH,"2017-6","2018-1")
 par(mfrow=c(2,1))
 plot(pcETH[,c(2,4,6,7,10)])
 plot(pcETH[,c(1)])
+abline(h=70)
 
 
 
--- StockCalc/R/CryptoCalc.R
+++ StockCalc/R/CryptoCalc.R
@@ -19,17 +19,18 @@ library("TTR")
 library("dygraphs")
 
 #------------------------------Functions-----------------------------#
-#1.1 Read Database
+
+#-----------------------1.1 Read Database----------------------------#
 Read_Crypt_DB<- function(Crypt_Name){
   Crypt_Add = paste("BITFINEX/",Crypt_Name,"USD", sep ="")
   Crypt_DB <- Quandl(Crypt_Add,type="xts")
   return (Crypt_DB)
 }
 
-#1.2 Calulate Stuff
+#----------------------1.2 Calulate Stuff----------------------------#
 Calc_Crypt <- function (Crypt_DB){
   
-  #1.2.1 MACD / EMA
+  #-----------------------1.2.1 MACD / EMA-----------------------------#
   # MACD 
   # Long Buy Signal : EMA12>EMA26 cross from below
   # Sell Signal     : EMA12<EMA26 cross from top
@@ -40,28 +41,31 @@ Calc_Crypt <- function (Crypt_DB){
   colnames(EMA)<- c("EMA12","EMA26")
   cCrypt_DB<-cbind.xts(EMA,Crypt_DB) 
   
-  #1.2.2 Boilinger Band
+  #---------------------1.2.2 Boilinger Band---------------------------#
   # Trend up    : Chart crosses Upper band
   # Trend low   : Chart crosses Lower band
   # Trend Change short: Chart close to one band -> leads to other direction 
   # Trend Change(up or down) : bandwidth small
-  
   BB20<- BBands(Crypt_DB[,3], sd=2.0)
   cCrypt_DB<-cbind.xts(BB20,cCrypt_DB) 
   
-  #1.2.3 RSI
+  #--------------------------1.2.3 RSI---------------------------------#
   # Undervalued   : RSI< 30
   # Overvalued    : RSI> 70
   Rsi<-RSI(Crypt_DB[,3])
   cCrypt_DB<-cbind.xts(Rsi,cCrypt_DB)
   
-  #1.2.X Return XTS Object
+  #--------------1.2.X Return XTS Object-------------------------------#
   # Return Object with following Structure:
   # EMA|BBdown|BBAVG|BBup|pctB|EMA12|EMA26|HIGH|LOW|MID|LAST|BID|ASK|Vol
   return (cCrypt_DB)
 }
 
-#1.4 Plot Range and Save
+#--------------------1.3 BuySignal-----------------------------------#
+# Algorithm to calculate WinSuccess 
+# Include BB(Bandwith small)+ RSI(70-30)+MACD(positiv)+BB(more uptrend)
+
+#----------------1.4 Plot Range and Save-----------------------------#
 Plot_Crypt <- function (cCrypt_DB, begDate, endDate){
   Date_Add = paste(begDate,"/",endDate, sep = "")
   plot_cCrypt_DB <- cCrypt_DB[Date_Add]
@@ -72,4 +76,3 @@ Plot_Crypt <- function (cCrypt_DB, begDate, endDate){
   # b<-plot(plot_cCrypt_DB[,c(1,2)])
   return(plot_cCrypt_DB)
 }
-
