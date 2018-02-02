a49e0d6593b9b98a4de80243a55b717630475bd8
mlacher <mlacher@live.de>
2018-02-02 19:52:53 +0100

    1st algorithm initiated

--- StockCalc/R/CryptoCalc.R
+++ StockCalc/R/CryptoCalc.R
@@ -21,58 +21,59 @@ library("dygraphs")
 #------------------------------Functions-----------------------------#
 
 #-----------------------1.1 Read Database----------------------------#
-Read_Crypt_DB<- function(Crypt_Name){
-  Crypt_Add = paste("BITFINEX/",Crypt_Name,"USD", sep ="")
-  Crypt_DB <- Quandl(Crypt_Add,type="xts")
-  return (Crypt_DB)
+Read_Share_DB<- function(Share_Type,Share_Name){
+  Share_Add = paste(Share_Type,Share_Name, sep ="")
+  Share_DB <- Quandl(Share_Add,type="xts")
+  return (Share_DB)
 }
 
 #----------------------1.2 Calulate Stuff----------------------------#
-Calc_Crypt <- function (Crypt_DB){
+Calc_Share <- function (Share_DB){
   
   #-----------------------1.2.1 MACD / EMA-----------------------------#
   # MACD 
   # Long Buy Signal : EMA12>EMA26 cross from below
   # Sell Signal     : EMA12<EMA26 cross from top
-  macd  <- MACD(Crypt_DB[,2], 12, 26, 9, maType="EMA" )
-  EMA12 <- EMA(Crypt_DB[,3], 12)
-  EMA26 <- EMA(Crypt_DB[,3], 20)
+  macd  <- MACD(Share_DB[,2], 12, 26, 9, maType="EMA" )
+  EMA12 <- EMA(Share_DB[,3], 12)
+  EMA26 <- EMA(Share_DB[,3], 20)
   EMA <- cbind(EMA12,EMA26)
   colnames(EMA)<- c("EMA12","EMA26")
-  cCrypt_DB<-cbind.xts(EMA,Crypt_DB) 
+  cShare_DB<-cbind.xts(EMA,Share_DB) 
   
   #---------------------1.2.2 Boilinger Band---------------------------#
   # Trend up    : Chart crosses Upper band
   # Trend low   : Chart crosses Lower band
   # Trend Change short: Chart close to one band -> leads to other direction 
   # Trend Change(up or down) : bandwidth small
-  BB20<- BBands(Crypt_DB[,3], sd=2.0)
-  cCrypt_DB<-cbind.xts(BB20,cCrypt_DB) 
+  BB20<- BBands(Share_DB[,3], sd=2.0)
+  cShare_DB<-cbind.xts(BB20,cShare_DB) 
   
   #--------------------------1.2.3 RSI---------------------------------#
   # Undervalued   : RSI< 30
   # Overvalued    : RSI> 70
-  Rsi<-RSI(Crypt_DB[,3])
-  cCrypt_DB<-cbind.xts(Rsi,cCrypt_DB)
+  Rsi<-RSI(Share_DB[,3])
+  cShare_DB<-cbind.xts(Rsi,cShare_DB)
   
   #--------------1.2.X Return XTS Object-------------------------------#
   # Return Object with following Structure:
-  # EMA|BBdown|BBAVG|BBup|pctB|EMA12|EMA26|HIGH|LOW|MID|LAST|BID|ASK|Vol
-  return (cCrypt_DB)
+  # RSI|BBdown|BBAVG|BBup|pctB|EMA12|EMA26|HIGH|LOW|MID|LAST|BID|ASK|Vol
+  return (cShare_DB)
 }
 
 #--------------------1.3 BuySignal-----------------------------------#
 # Algorithm to calculate WinSuccess 
-# Include BB(Bandwith small)+ RSI(70-30)+MACD(positiv)+BB(more uptrend)
+#Normaliser BB Band
+#Alg_DB<- cbind.xts(cShare_DB[,3]/cShare_DB[,3],cShare_DB[,2]/cShare_DB[,3],cShare_DB[,4]/cShare_DB[,3])
 
 #----------------1.4 Plot Range and Save-----------------------------#
-Plot_Crypt <- function (cCrypt_DB, begDate, endDate){
+Plot_Share <- function (cShare_DB, begDate, endDate){
   Date_Add = paste(begDate,"/",endDate, sep = "")
-  plot_cCrypt_DB <- cCrypt_DB[Date_Add]
+  plot_cShare_DB <- cShare_DB[Date_Add]
   
   
   # par(mfrow=c(2,1))
-  # a<-plot(plot_cCrypt_DB[,3])
-  # b<-plot(plot_cCrypt_DB[,c(1,2)])
-  return(plot_cCrypt_DB)
+  # a<-plot(plot_cShare_DB[,3])
+  # b<-plot(plot_cShare_DB[,c(1,2)])
+  return(plot_cShare_DB)
 }
