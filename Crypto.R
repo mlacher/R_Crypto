b931d9696bcd35ccf4528ed0a0d758a6cf7d12b8
mlacher <mlacher@live.de>
2018-02-03 00:30:52 +0100

    added time and delete pceth
    faster and more accuracy

--- Crypto.R
+++ Crypto.R
@@ -17,18 +17,21 @@ library("dygraphs")
 library("StockCalc")
 Quandl.api_key("aFFTC-nfXbcUNY5xbuVt")
 #user mxlchr, PW krass123
+Starttime <- Sys.time()
 hallo <- c("AAPL","AES","ADI","CCE")
-buy <- c(0,0,0)
-sell <- c(0,0,0)
-for (b in 1:4){
+buy <- c(0)
+sell <- c(0)
+array_size <- length(hallo)
+for (b in 1:array_size){
  
   
-ETH  <- Read_Share_DB("WIKI/",hallo[b])
+ETH  <- Read_Share_DB("WIKI/",hallo[b],"2017-06-30","2018-01-31")
 cETH <- Calc_Share(ETH)
-pcETH <- Plot_Share(cETH,"2017-6","2018-2")
+cETH<-na.omit(cETH)
+
 
 #BB band calculations
-nETC<- cbind.xts(pcETH[,3]/pcETH[,3],pcETH[,2]/pcETH[,3],pcETH[,4]/pcETH[,3],0,0,0,0)
+nETC<- cbind.xts(cETH[,3]/cETH[,3],cETH[,2]/cETH[,3],cETH[,4]/cETH[,3],0,0,0,0)
 array_size <- length(nETC [,3])
 #1 or 0 if below threshold
 for(i in 1:array_size){
@@ -54,7 +57,7 @@ for(i in 1:(array_size-1)){
   else{
     nETC[(i+1),5] <- 0;
   } 
-  if (pcETH[(i+1),10]>pcETH[(i+1),4]){ #Value hits UpperBB
+  if (cETH[(i+1),10]>cETH[(i+1),4]){ #Value hits UpperBB
     nETC[(i+1),7]<- 1;
   }
   else {
@@ -62,24 +65,26 @@ for(i in 1:(array_size-1)){
   }
   
   if ((nETC[(i+1),7]==1) && (nETC[(i+1),6]==1)&& (goto ==0)){ # first test buy in
-    buy[b] <- pcETH[(i+1),10];
+    buy[b] <- cETH[(i+1),10];
     i = (array_size-1);# does not work?
     goto = 1;
-    sell[b] <- pcETH[(array_size),10];
+    sell[b] <- cETH[(array_size),10];
   }
 
 }
 
 
 }
-
+Stoptime <- Sys.time()
+print(Stoptime-Starttime)
 # Plot
-par(mfrow=c(3,1))
-plot(pcETH[,c(2,4,6,7,10)])
+plot(sell/buy)
+par(mfrow=c(4,1))
+plot(cETH[,c(2,4,6,7,10)])
 plot(nETC[,c(1:3)])
 plot(nETC[,c(6)])
 plot(nETC[,c(7)])
-plot(pcETH[,c(1)])
+plot(cETH[,c(1)])
 #BBand based on the mean_norm value
 
 plot(nETC)
--- StockCalc/R/CryptoCalc.R
+++ StockCalc/R/CryptoCalc.R
@@ -21,17 +21,18 @@ library("dygraphs")
 #------------------------------Functions-----------------------------#
 
 #-----------------------1.1 Read Database----------------------------#
-Read_Share_DB<- function(Share_Type,Share_Name){
+Read_Share_DB<- function(Share_Type,Share_Name,begDate, endDate){
   Share_Add = paste(Share_Type,Share_Name, sep ="")
-  Share_DB <- Quandl(Share_Add,type="xts")
+  Share_DB <- Quandl(Share_Add,type="xts",
+                     start_date=begDate, end_date=endDate)
   return (Share_DB)
 }
 
 #----------------------1.2 Calulate Stuff----------------------------#
 Calc_Share <- function (Share_DB){
-  
+
   #-----------------------1.2.1 MACD / EMA-----------------------------#
-  # MACD 
+  # MACD
   # Long Buy Signal : EMA12>EMA26 cross from below
   # Sell Signal     : EMA12<EMA26 cross from top
   macd  <- MACD(Share_DB[,2], 12, 26, 9, maType="EMA" )
@@ -39,41 +40,33 @@ Calc_Share <- function (Share_DB){
   EMA26 <- EMA(Share_DB[,3], 20)
   EMA <- cbind(EMA12,EMA26)
   colnames(EMA)<- c("EMA12","EMA26")
-  cShare_DB<-cbind.xts(EMA,Share_DB) 
-  
+  cShare_DB<-cbind.xts(EMA,Share_DB)
+
   #---------------------1.2.2 Boilinger Band---------------------------#
   # Trend up    : Chart crosses Upper band
   # Trend low   : Chart crosses Lower band
-  # Trend Change short: Chart close to one band -> leads to other direction 
+  # Trend Change short: Chart close to one band -> leads to other direction
   # Trend Change(up or down) : bandwidth small
   BB20<- BBands(Share_DB[,3], sd=2.0)
-  cShare_DB<-cbind.xts(BB20,cShare_DB) 
-  
+  cShare_DB<-cbind.xts(BB20,cShare_DB)
+
   #--------------------------1.2.3 RSI---------------------------------#
   # Undervalued   : RSI< 30
   # Overvalued    : RSI> 70
   Rsi<-RSI(Share_DB[,3])
   cShare_DB<-cbind.xts(Rsi,cShare_DB)
-  
+
   #--------------1.2.X Return XTS Object-------------------------------#
   # Return Object with following Structure:
   # RSI|BBdown|BBAVG|BBup|pctB|EMA12|EMA26|HIGH|LOW|MID|LAST|BID|ASK|Vol
+  cShare_DB<- na.omit(cShare_DB) # remove mising values
   return (cShare_DB)
 }
 
-#--------------------1.3 BuySignal-----------------------------------#
-# Algorithm to calculate WinSuccess 
+#--------------------1.3 BuySignal-------------------------------------#
+# Algorithm to calculate WinSuccess
 #Normaliser BB Band
 #Alg_DB<- cbind.xts(cShare_DB[,3]/cShare_DB[,3],cShare_DB[,2]/cShare_DB[,3],cShare_DB[,4]/cShare_DB[,3])
 
-#----------------1.4 Plot Range and Save-----------------------------#
-Plot_Share <- function (cShare_DB, begDate, endDate){
-  Date_Add = paste(begDate,"/",endDate, sep = "")
-  plot_cShare_DB <- cShare_DB[Date_Add]
-  
-  
-  # par(mfrow=c(2,1))
-  # a<-plot(plot_cShare_DB[,3])
-  # b<-plot(plot_cShare_DB[,c(1,2)])
-  return(plot_cShare_DB)
-}
+#----------------1.4 tbd-----------------------------------------------#
+
