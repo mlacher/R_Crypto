9594dd0ba189482a751fd93c05a3a83954f61b6b
mlacher <mlacher@live.de>
2018-02-07 17:49:02 +0100

    added calc to library

--- Crypto.R
+++ Crypto.R
@@ -7,7 +7,7 @@ install.packages("dygraphs")
 install.packages("digest")
 install.packages("TTR")
 
-library("xts")  
+library("xts")
 library("Quandl")
 library("TTR")
 library("dygraphs")
@@ -50,41 +50,28 @@ sell <- data.frame(matrix(vector(), 0, 1,
                           dimnames=list(c(), c("Sell"))),
                    stringsAsFactors=F)
 for (b in 1:array_size){
- 
- 
-ETH  <- Read_Share_DB("WIKI/",sh_names[b],"2017-06-30","2018-01-31")
+
+
+ETH  <- Read_Share_DB("WIKI/",sh_names[1],"2017-06-30","2018-01-31")
 cETH <- Calc_Share(ETH)
+nETC<-Eval_Share(cETH )
 
-
-
-#BB band calculations
-nETC<- cbind.xts(cETH[,3]/cETH[,3],cETH[,2]/cETH[,3],cETH[,4]/cETH[,3],0,0,0,0)
-array_size <- length(nETC [,3])
-#1 or 0 if below threshold
-for(i in 1:array_size){
-  if(nETC [i,3]< 1.04){                
-    nETC[i,4]<- 1;
-  }
-  else{
-    nETC[i,4] <- 0;
-  }                    
-}
 
 #1 or 0 if below x days
 goto = 0;
 for(i in 1:(array_size-1)){
-  if(nETC [(i+1),4]>0){  #bigger 1.035              
+  if(nETC [(i+1),4]>0){  #bigger 1.035
     nETC[(i+1),5]<- as.numeric(nETC [i,5])+as.numeric(nETC [(i+1),4]);
     if (nETC[(i+1),5]>10){ # 15Days value below TH
       nETC[(i+1),6]<- 1;
       #nETC[(i+1),5]<-0; # RESET
     } # First Buy Sign @ 15D
     else {nETC[(i+1),6]<- 0;}
-    
+
   }
   else{
     nETC[(i+1),5] <- 0;
-  } 
+  }
   if (cETH[(i+1),10]>cETH[(i+1),4]){ #Value hits UpperBB (buy signal)
     nETC[(i+1),7]<- 1;
   }
@@ -94,14 +81,14 @@ for(i in 1:(array_size-1)){
   else {
     nETC[(i+1),7]<- 0;
   }
-  
+
   if ((nETC[(i+1),7]==1) && (nETC[(i+1),6]==1)&& (goto ==0)){ # first test buy in
     buy<- rbind.data.frame(cETH[(i+1),10],buy) ;
     goto = 1;
     sell <- rbind.data.frame(cETH[(array_size),10],sell);
   }
 
-  
+
 }
 
 
@@ -118,7 +105,7 @@ plot(nETC[,c(6)])
 plot(nETC[,c(7)])
 plot(nETC[,c(1:3)])
 
-dygraph(cETH[,c(2,4,6,7,10)]) %>% 
+dygraph(cETH[,c(2,4,6,7,10)]) %>%
   # dyRangeSelector()
   dyAnnotation(rownames(buy)[1], text = "B", tooltip = "Buy")
 
---
+++ StockCalc/R/CalcShare.R
@@ -0,0 +1,21 @@
+
+#Boilinger Band evaluation
+Eval_Share <- function (cShare_DB){
+
+#Normalise BBand to mean value
+cCalc_Share_DB<- cbind.xts(cShare_DB[,3]/cShare_DB[,3],
+                 cShare_DB[,2]/cShare_DB[,3],
+                 cShare_DB[,4]/cShare_DB[,3],
+                 0,0,0,0)
+array_size <- length(cCalc_Share_DB [,3])
+#1 or 0 if below threshold
+for(i in 1:array_size){
+  if(cCalc_Share_DB [i,3]< 1.04){
+    cCalc_Share_DB[i,4]<- 1;
+  }
+  else{
+    cCalc_Share_DB[i,4] <- 0;
+  }
+}
+return(cCalc_Share_DB)
+}
