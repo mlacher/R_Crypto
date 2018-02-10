8de25b67021088c258820b8ba3c3c8dea4dfc941
mlacher <mlacher@live.de>
2018-02-10 23:07:36 +0100

    added minor changes
    next code review necessary

--- Crypto.R
+++ Crypto.R
@@ -19,41 +19,50 @@ library("StockCalc")
 Quandl.api_key("aFFTC-nfXbcUNY5xbuVt")
 #user mxlchr, PW krass123
 Starttime <- Sys.time()
-# sh_names <- c("AES","ADI","CCE","DOW","EW",
-#               "FISV",
-#               "HD",
-#               "FDX",
-#               "ICE",
-#               "FLS",
-#               "IR",
-#               "IP",
-#               "FMC",
-#               "FFIV",
-#               "FOXA",
-#               "FIS",
-#               "FITB",
-#               "MMM",
-#               "NI",
-#               "FTI",
-#               "F",
-#               "AAPL",
-#               "FB",
-#               "AMZN"
-# )
-sh_names <- c("AMZN","MMM",
-                             "NI",
-                            "FTI",
-                            "F","ICE",
-                            "FLS","FIS",
-                            "FITB")
+sh_names <- c("AES","ADI","DOW","EW",
+              "FISV",
+              "HD",
+              "FDX",
+              "ICE",
+              "FLS",
+              "IR",
+              "IP",
+              "FMC",
+              "FFIV",
+              "FOXA",
+              "FIS",
+              "FITB",
+              "MMM",
+              "NI",
+              "FTI",
+              "F",
+              "AAPL",
+              "FB",
+              "AMZN",
+              "MUSA",
+              "MVC",
+              "MYE",
+              "MYRG",
+              "MYGN",
+              "NBTB",
+              "NC",
+              "NANO",
+              "NSTG",
+              "NNVC",
+              "NATH"
+             
+              
+)
+
 array_size <- length(sh_names)
 
-buy <- data.frame(matrix(vector(), 0, 1,
-                       dimnames=list(c(), c("Buy"))),
+buy <- data.frame(matrix(vector(), 0, 2,
+                       dimnames=list(c(), c("Buy","Name"))),
                 stringsAsFactors=F)
 sell <- data.frame(matrix(vector(), 0, 1,
                           dimnames=list(c(), c("Sell"))),
                    stringsAsFactors=F)
+
 for (b in 1:array_size){
 
 
@@ -88,14 +97,16 @@ for(i in 1:(array_size-1)){
   }
 
   if ((nETC[(i+1),7]==1) && (nETC[(i+1),6]==1)&& (goto ==0)){ # first test buy in
-    buy<- rbind.data.frame(cETH[(i+7),10],buy) ; # +7, often price drop after crossing UpperBB
+    buy<- rbind.data.frame(cETH[(i+5),10],buy) ; # +5, often price drop after crossing UpperBB
     goto = 1;
     sell <- rbind.data.frame(cETH[(array_size),10],sell);
   }
-
-
 }
 
+if (goto == 0){
+  sell <- rbind.data.frame(cETH[(array_size),10],sell)
+  buy <- rbind.data.frame(cETH[(array_size),10],buy)
+  }
 
 }
 Stoptime <- Sys.time()
@@ -110,7 +121,7 @@ layout(mat=matrix(c(1,2,3,4),nrow=4,ncol=1,byrow=T))
 plot.xts(cETH[,c(2,4,6,7,10)])
 plot(nETC[,c(6)])
 plot(nETC[,c(7)])
-#plot(nETC[,c(1:3)])
+plot(nETC[,c(1:3)])
 
 dygraph(cETH[,c(2,4,6,7,10)]) %>%
   # dyRangeSelector()
@@ -118,6 +129,6 @@ dygraph(cETH[,c(2,4,6,7,10)]) %>%
   dyAnnotation(rownames(sell)[1], text = "S", tooltip = "Sell")
 
 #BBand based on the mean_norm value
-plot(nETC)
+
 
 
