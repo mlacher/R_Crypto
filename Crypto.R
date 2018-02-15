e3cc15c7e03277fdeec374c9d7ab60c49ed1ce0b
Maximilian Lacher <Maximilian.Lacher@vectura.com>
2018-02-15 14:52:18 +0100

    Revert "1st major update"
    This reverts commit 84834cff5f571e77695ae1eb2c2cc73238184504.

--- Crypto.R
+++ Crypto.R
@@ -51,17 +51,18 @@ sh_names <- c("AES","ADI","DOW","EW",
               "NSTG",
               "NNVC",
               "NATH"
-
-
+             
+              
 )
 
-#array_size <- length(sh_names)
-array_size <-3 # debug function
-
+array_size <- length(sh_names)
+#array_size <-3 # debug function
 buy <- data.frame(matrix(vector(), 0, 2,
-                         dimnames=list(c(), c("Buy","Name"))),
-                  stringsAsFactors=F)
-
+                       dimnames=list(c(), c("Buy","Name"))),
+                stringsAsFactors=F)
+sell <- data.frame(matrix(vector(), 0, 1,
+                          dimnames=list(c(), c("Sell"))),
+                   stringsAsFactors=F)
 
 allShare <- xts(x="", order.by=Sys.Date())
 =======
@@ -102,8 +103,10 @@ for (b in 1:array_size){
 
 <<<<<<< HEAD
 ETH  <- Read_Share_DB("WIKI/",sh_names[b],"2017-06-30","2018-01-31")
+
 allShare<- cbind(allShare,ETH[,3])
 cETH <- Calc_Share_DB(ETH)
+<<<<<<< HEAD
 buy<-Eval_Share(cETH,buy )
 =======
 ETH  <- Read_Share_DB("WIKI/",sh_names[1],"2017-06-30","2018-01-31")
@@ -143,17 +146,55 @@ for(i in 1:(array_size-1)){
   }
 >>>>>>> parent of 2c14de5... code cleaning
 
+=======
+nETH<-Eval_Share(cETH )
 
+array_size <- length(nETH[,3])
+#1 or 0 if below x days
+goto = 0;
+for(i in 1:(array_size-1)){
+  if(nETH [(i+1),4]>0){  #bigger 1.035
+    nETH[(i+1),5]<- as.numeric(nETH [i,5])+as.numeric(nETH [(i+1),4]);
+    if (nETH[(i+1),5]>15){ # 15Days value below TH
+      nETH[(i+1),6]<- 1;
+      #nETH[(i+1),5]<-0; # RESET
+    } # First Buy Sign @ 15D
+    else {nETH[(i+1),6]<- 0;}
+
+  }
+  else{
+    nETH[(i+1),5] <- 0;
+  }
+  if (cETH[(i+1),10]>cETH[(i+1),4]){ #Value hits UpperBB (buy signal)
+    nETH[(i+1),7]<- 1;
+  }
+  else if (cETH[(i+1),10]<cETH[(i+1),2]){ #Value hits LowerBB (sell signal)
+    nETH[(i+1),7]<- -1;
+  }
+  else {
+    nETH[(i+1),7]<- 0;
+  }
+
+  if ((nETH[(i+1),7]==1) && (nETH[(i+1),6]==1)&& (goto ==0)){ # first test buy in
+    buy<- rbind.data.frame(cETH[(i+5),10],buy) ; # +5, often price drop after crossing UpperBB
+    goto = 1;
+    sell <- rbind.data.frame(cETH[(array_size),10],sell);
+  }
+}
+>>>>>>> parent of 84834cf... 1st major update
+
+if (goto == 0){
+  sell <- rbind.data.frame(cETH[(array_size),10],sell)
+  buy <- rbind.data.frame(cETH[(array_size),10],buy)
+  }
 
 }
-#delte first dummy value
-allShare <- allShare[,-1]
-colnames(allShare)<- sh_names
-
 Stoptime <- Sys.time()
 print(Stoptime-Starttime)
 
-
+#delte first dummy value
+allShare <- allShare[,-1]
+colnames(allShare)<- sh_names
 
 
 plot(sell/buy)
--- StockCalc/R/CalcShare.R
+++ StockCalc/R/CalcShare.R
@@ -1,10 +1,15 @@
 
+<<<<<<< HEAD:StockCalc/R/CalcShare.R
 Eval_Share <- function (cShare_DB,cbuy){
 
   sell <- data.frame(matrix(vector(), 0, 1,
                             dimnames=list(c(), c("Sell"))),
                      stringsAsFactors=F)
 
+=======
+#Boilinger Band evaluation
+Eval_Share <- function (cShare_DB){
+>>>>>>> parent of 84834cf... 1st major update:StockCalc/R/AlgShare_V1.R
 
 #Normalise BBand to mean value
 cCalc_Share_DB<- cbind.xts(cShare_DB[,3]/cShare_DB[,3],
@@ -21,39 +26,5 @@ for(i in 1:array_size){
     cCalc_Share_DB[i,4] <- 0;
   }
 }
-
-
-array_size <- length(cCalc_Share_DB[,3])
-#1 or 0 if below x days
-goto = 0;
-for(i in 1:(array_size-1)){
-  if(cCalc_Share_DB [(i+1),4]>0){  #bigger 1.035
-    cCalc_Share_DB[(i+1),5]<- as.numeric(cCalc_Share_DB [i,5])+as.numeric(cCalc_Share_DB [(i+1),4]);
-    if (cCalc_Share_DB[(i+1),5]>15){ # 15Days value below TH
-      cCalc_Share_DB[(i+1),6]<- 1;
-      #cCalc_Share_DB[(i+1),5]<-0; # RESET
-    } # First cbuy Sign @ 15D
-    else {cCalc_Share_DB[(i+1),6]<- 0;}
-
-  }
-  else{
-    cCalc_Share_DB[(i+1),5] <- 0;
-  }
-  if (cShare_DB[(i+1),10]>cShare_DB[(i+1),4]){ #Value hits UpperBB (cbuy signal)
-    cCalc_Share_DB[(i+1),7]<- 1;
-  }
-  else if (cShare_DB[(i+1),10]<cShare_DB[(i+1),2]){ #Value hits LowerBB (sell signal)
-    cCalc_Share_DB[(i+1),7]<- -1;
-  }
-  else {
-    cCalc_Share_DB[(i+1),7]<- 0;
-  }
-
-  if ((cCalc_Share_DB[(i+1),7]==1) && (cCalc_Share_DB[(i+1),6]==1)&& (goto ==0)){ # first test cbuy in
-    cbuy<- rbind.data.frame(cShare_DB[(i+5),10],cbuy) ; # +5, often price drop after crossing UpperBB
-    goto = 1;
-    sell <- rbind.data.frame(cShare_DB[(array_size),10],sell);
-  }
-}
-return(cbuy)
+return(cCalc_Share_DB)
 }
